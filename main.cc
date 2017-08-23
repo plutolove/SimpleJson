e9de22e0a338d8fae712cadb2ad1901e0172b983
plutolove <mengshangqis@gmail.com>
2017-08-23 10:51:49 +0800

    format code

--- main.cc
+++ main.cc
@@ -1,21 +1,19 @@
+#include <json.h>
 #include <iostream>
 #include <string>
-#include <json.h>
 using namespace std;
 using meng::Json;
 int main() {
-	string fname = "../tests/test.json";
-	meng::Json tmp = parse_file(fname.c_str());
-	cout<<tmp["editor.fontFamily"].string_value()<<endl;
-	cout<<tmp["code-runner.executorMap"]["java"].string_value()<<endl;
-	cout<<tmp["python.linting.pylintArgs"][0].string_value()<<endl;
-	cout<<tmp["editor.fontSize"].int_value()<<endl;
-	Json my_json = Json::object {
-		{ "key1", "value1" },
-		{ "key2", false },
-		{ "key3", Json::array { 1, 2, 3 } },
-	};
-	std::string json_str = my_json.dump();
-	cout<<json_str<<endl;
-	return 0;
+  string fname = "../tests/test.json";
+  meng::Json tmp = parse_file(fname.c_str());
+  cout << tmp["editor.fontFamily"].string_value() << endl;
+  cout << tmp["code-runner.executorMap"]["java"].string_value() << endl;
+  cout << tmp["python.linting.pylintArgs"][0].string_value() << endl;
+  cout << tmp["editor.fontSize"].int_value() << endl;
+  Json my_json = Json::object{
+      {"key1", "value1"}, {"key2", false}, {"key3", Json::array{1, 2, 3}},
+  };
+  std::string json_str = my_json.dump();
+  cout << json_str << endl;
+  return 0;
 }
--- tests/main-test.cc
+++ tests/main-test.cc
@@ -3,26 +3,27 @@
 #include "json.h"
 using namespace std;
 TEST(Parser, File) {
-    string fname = "../tests/test.json";
-    meng::Json tmp = parse_file(fname.c_str());
-    ASSERT_EQ(tmp["editor.fontFamily"].string_value(), "Monaco");
-    ASSERT_EQ(tmp["code-runner.executorMap"]["java"].string_value(), "cd $dir && javac $fileName && java $fileNameWithoutExt");
-    ASSERT_EQ(tmp["python.linting.pylintArgs"][0].string_value(), "-d c0103, c0111");
-    ASSERT_EQ(tmp["editor.fontSize"].int_value(), 20);
+  string fname = "../tests/test.json";
+  meng::Json tmp = parse_file(fname.c_str());
+  ASSERT_EQ(tmp["editor.fontFamily"].string_value(), "Monaco");
+  ASSERT_EQ(tmp["code-runner.executorMap"]["java"].string_value(),
+            "cd $dir && javac $fileName && java $fileNameWithoutExt");
+  ASSERT_EQ(tmp["python.linting.pylintArgs"][0].string_value(),
+            "-d c0103, c0111");
+  ASSERT_EQ(tmp["editor.fontSize"].int_value(), 20);
 }
 
 TEST(ToJson, object) {
-    using meng::Json;
-    Json my_json = Json::object {
-		{ "key1", "value1" },
-		{ "key2", false },
-		{ "key3", Json::array { 1, 2, 3 } },
-	};
-    std::string json_str = my_json.dump();
-    ASSERT_EQ(json_str, "{\"key1\": \"value1\", \"key2\": false, \"key3\": [1, 2, 3]}");
+  using meng::Json;
+  Json my_json = Json::object{
+      {"key1", "value1"}, {"key2", false}, {"key3", Json::array{1, 2, 3}},
+  };
+  std::string json_str = my_json.dump();
+  ASSERT_EQ(json_str,
+            "{\"key1\": \"value1\", \"key2\": false, \"key3\": [1, 2, 3]}");
 }
 int main(int argc, char** argv) {
-    testing::InitGoogleTest(&argc, argv);
-    RUN_ALL_TESTS();
-    return 0;
+  testing::InitGoogleTest(&argc, argv);
+  RUN_ALL_TESTS();
+  return 0;
 }
